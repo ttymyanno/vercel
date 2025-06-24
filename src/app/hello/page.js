b51c96d81a7132935cac421663c66ec46c48a65a
yann <yann.touassom@gmail.com>
2025-06-24 11:15:18 +0200

    Ajout de la page hello

--- src/app/hello/page.js
+++ src/app/hello/page.js
@@ -4,7 +4,7 @@ export default function HelloPage() {
   return (
     <main style={{ padding: "2rem", textAlign: "center" }}>
       <h1>Bienvenue sur ma page Next.js ! 🚀</h1>
-      <p>Je suis YANN TOUASSOM</p>
+      <p>Je suis YANN TOUASSOM MORREL ETUDIANT A PARIS</p>
     </main>
   );
 }
