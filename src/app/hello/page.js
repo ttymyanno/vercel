b2293827e9b013522ef021539423b3936866aab7
yann <yann.touassom@gmail.com>
2025-06-24 11:00:51 +0200

    Ajout de la page hello

---
+++ src/app/hello/page.js
@@ -0,0 +1,10 @@
+// app/hello/page.js
+
+export default function HelloPage() {
+  return (
+    <main style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bienvenue sur ma page Next.js ! 🚀</h1>
+      <p>Ceci est une page de test déployée sur Vercel.</p>
+    </main>
+  );
+}
